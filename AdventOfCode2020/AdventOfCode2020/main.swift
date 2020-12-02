2085b44ece882c815444291dcade107b2e6f4232
bart-kneepkens <bkneepkens@msn.com>
2020-12-02 11:24:36 +0100

    Day 2

--- AdventOfCode2020/AdventOfCode2020/main.swift
+++ AdventOfCode2020/AdventOfCode2020/main.swift
@@ -1,5 +1,7 @@
 import Foundation
 
-print(Day1Hashing().part1()) // 440979
-print(Day1Hashing().part2()) // 82498112
+//print(Day1Hashing().part1()) // 440979
+//print(Day1Hashing().part2()) // 82498112
+
+Day2().solve2()
 
