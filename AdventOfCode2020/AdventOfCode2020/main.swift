e8434e59434e0405bdc8082bdd6708492944b2da
Bart Kneepkens <bkneepkens@msn.com>
2020-12-07 22:35:35 +0100

    Day 7

--- AdventOfCode2020/AdventOfCode2020/main.swift
+++ AdventOfCode2020/AdventOfCode2020/main.swift
@@ -1,4 +1,12 @@
 import Foundation
 
-print(Day6.solve() == 6416) // 6416
-print(Day6.solve2() == 3050) // 3050
+//print(Day6.solve() == 6416) // 6416
+//let before = Date()
+//print(Day6.solve2() == 3050) // 3050
+//let after = Date()
+//
+//print(Day6.solve3())
+//print("took \(after.timeIntervalSince(before)) to run 2")
+
+print(Day7.solve()) // 119
+print(Day7.solve2()) // 155802
