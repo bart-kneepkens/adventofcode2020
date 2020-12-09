91a04e3d15377b407c342f26057d62a90356fb68
Bart Kneepkens <bkneepkens@msn.com>
2020-12-09 22:40:52 +0100

    A brute-force solution for day 9

--- AdventOfCode2020/AdventOfCode2020/main.swift
+++ AdventOfCode2020/AdventOfCode2020/main.swift
@@ -1,12 +1,4 @@
 import Foundation
 
-//print(Day6.solve() == 6416) // 6416
-//let before = Date()
-//print(Day6.solve2() == 3050) // 3050
-//let after = Date()
-//
-//print(Day6.solve3())
-//print("took \(after.timeIntervalSince(before)) to run 2")
-
-print(Day8.solve() == 1451) // 1451
-print(Day8.solve2() == 1160) // 1160
+print(Day9.solve()) // 257342611
+print(Day9.solve2()) // 35602097
